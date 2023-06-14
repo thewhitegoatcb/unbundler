00fcbfc952217040e02053c275ed11bb9cc9b38f
WhiteGoat <WhiteGoat@WhiteGoat>
2023-06-14 13:06:21 +0300

    remove

--- unbundler/file_entry.py
+++
@@ -1,6 +0,0 @@
-class FileEntry:
-    def __init__(self, ext_hash, filename_hash, file_type, file_size) -> None:
-        self.ext_hash = ext_hash
-        self.filename_hash = filename_hash
-        self.type = file_type
-        self.file_size = file_size
